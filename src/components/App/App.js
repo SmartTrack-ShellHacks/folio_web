566a71a15375c4c8416dc7cbec139d25096435ae
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-09-25 16:58:48 -0700

    fixes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Portfolio from "../Portfolio/Portfolio";
-
+import Market from '../Market/Market'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-
+        <Market/>
         <Routes>
           <Route path="/" />
           <Route path="/addPortfolio" />
