157be62517e3175666371b53eccf0768059fcf7d
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-09-26 03:50:27 -0700

    added flex to market

--- src/components/Market/Coins.js
+++ src/components/Market/Coins.js
@@ -1,18 +1,29 @@
 import React from "react";
 import Card from './Card'
+import { makeStyles } from "@material-ui/styles";
 
 const Coins = ({ portfolio, btcPrice, ethPrice, adaPrice, dogePrice, uniPrice, eosPrice, usdtPrice, xrpPrice }) =>{
     const coins =['btc', 'eth', 'ada', 'doge', 'usdt', 'uni', 'xrp', 'eos']
+    const useStyles = makeStyles({
+        cards: {
+          display: "flex",
+          flexFlow: "row wrap",
+          justifyContent: "space-evenly",
+          alignItems: 'center'
+        },
+
+    });
     
+    const classes = useStyles();
 
 
 
     return (
-        <>
+        <div className={classes.cards}>
             {coins.map((coin)=>{
                 return <Card coin={coin} key={coin}/>
             })}
-        </>
+        </div>
     )
 }
 
