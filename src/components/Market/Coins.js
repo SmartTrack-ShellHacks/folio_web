9593f66bd60105328291f64c822e67c9cb3bc1eb
Carlos Chavez <carlos@Carloss-MacBook-Pro.local>
2021-09-26 06:59:00 -0400

    market styles

--- src/components/Market/Coins.js
+++ src/components/Market/Coins.js
@@ -1,19 +1,15 @@
 import React from "react";
-import Card from './Card'
+import Card from "./Card";
 
-const Coins = ({ portfolio, btcPrice, ethPrice, adaPrice, dogePrice, uniPrice, eosPrice, usdtPrice, xrpPrice }) =>{
-    const coins =['btc', 'eth', 'ada', 'doge', 'usdt', 'uni', 'xrp', 'eos']
-    
-
-
-
-    return (
-        <>
-            {coins.map((coin)=>{
-                return <Card coin={coin} key={coin}/>
-            })}
-        </>
-    )
-}
+const Coins = ({ portfolio, btcPrice, ethPrice, adaPrice, dogePrice, uniPrice, eosPrice, usdtPrice, xrpPrice }) => {
+  const coins = ["btc", "eth", "ada", "doge", "usdt", "uni", "xrp", "eos"];
+  return (
+    <>
+      {coins.map((coin) => {
+        return <Card coin={coin} key={coin} />;
+      })}
+    </>
+  );
+};
 
 export default Coins;
--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,21 +1,34 @@
-
 import React from "react";
-import Header from './Header'
+import Header from "./Header";
 import Coins from "./Coins";
 import Navbar from "../Navbar/Navbar";
+import { Typography, makeStyles } from "@material-ui/core";
 
+const Market = () => {
+  const useStyles = makeStyles({
+    title: {
+      display: "flex",
+      justifyContent: "space-between",
+      marginTop: "2rem",
+      marginLeft: "2rem",
+      marginRight: "2rem",
+    },
+  });
 
+  const classes = useStyles();
 
-const Market = () =>{
-
-    return(
-        <>
-            <Navbar />
-            <span>Coin Market</span>
-            <Header />
-            <Coins />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <div className={classes.title}>
+        <Typography className={classes.text} variant="h3">
+          Market
+        </Typography>
+      </div>
+      <Header />
+      <Coins />
+    </>
+  );
+};
 
 export default Market;
