ff19bd8772751ab204bbe2a1f4f6c160a7f76688
Carlos Chavez <carlos@Carloss-MacBook-Pro.local>
2021-09-25 23:27:15 -0400

    added functionality to store data in local storage from adding portfolio page to the main portfolio view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,11 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Portfolio from "../Portfolio/Portfolio";
 import AddPortfolio from "../AddPortfolio/AddPortfolio";
-import Market from '../Market/Market'
-
+import Market from "../Market/Market";
 
 function App() {
-  const [portfolio, setPortfolio] = useState([
-    {
+  const [portfolio, setPortfolio] = useState({
+    Cardano: {
       coin: "Cardano",
       symbol: "ADA",
       icon: "https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/cardano_ada-512.png",
@@ -17,7 +16,7 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-    {
+    Bitcoin: {
       coin: "Bitcoin",
       symbol: "BTC",
       icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png",
@@ -26,7 +25,7 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-    {
+    Dogecoin: {
       coin: "Dogecoin",
       symbol: "DOGE",
       icon: "https://seeklogo.com/images/D/dogecoin-logo-625F9D262A-seeklogo.com.png",
@@ -35,7 +34,7 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-    {
+    Uniswap: {
       coin: "Uniswap",
       symbol: "UNI",
       icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHsV6-0KW9vH6bz-UfpscApA9udwHtJbL16-ddXTOVauyNLjqXm5e94vNs0Bc-h2JyQig&usqp=CAU",
@@ -44,7 +43,7 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-    {
+    Tether: {
       coin: "Tether",
       symbol: "USDT",
       icon: "https://cdn.iconscout.com/icon/free/png-256/tether-441954.png",
@@ -53,7 +52,7 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-    {
+    XRP: {
       coin: "XRP",
       symbol: "XRP",
       icon: "https://cdn.freelogovectors.net/wp-content/uploads/2021/01/xrp-icon-freelogovectors.net_.png",
@@ -62,7 +61,7 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-    {
+    Ethereum: {
       coin: "Ethereum",
       symbol: "ETH",
       icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
@@ -71,7 +70,7 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-    {
+    EOS: {
       coin: "EOS",
       symbol: "EOS",
       icon: "https://i.dlpng.com/static/png/6505264_preview.png",
@@ -80,14 +79,14 @@ function App() {
       amountBought: 0,
       totalGain: 0,
     },
-  ]);
+  });
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Portfolio portfolio={portfolio} />} />
-          <Route path="/addPortfolio" element={ <AddPortfolio /> } />
+          <Route path="/addPortfolio" element={<AddPortfolio />} />
           <Route path="/portfolio" />
         </Routes>
       </BrowserRouter>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,24 @@
 import { Icon, Grid, Typography, makeStyles } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import Chart from "../Market/Chart";
 import Navbar from "../Navbar/Navbar";
+import { useState, useEffect } from "react";
 import Card from "./Card";
+import axios from "axios";
 
 function Portfolio({ portfolio }) {
+  const [price, setPrice] = useState(2);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get("htps://rest.coinapi.io/v1/exchangerate/BTC/USD");
+      console.log(data);
+    };
+    getData();
+  }, []);
+
   const useStyles = makeStyles({
+    portfolio: {},
     title: {
       display: "flex",
       justifyContent: "space-between",
@@ -21,33 +35,57 @@ function Portfolio({ portfolio }) {
       marginTop: "3rem",
     },
     wrap: {
-      marginTop: "3rem",
+      marginTop: "2rem",
+      marginBottom: "2rem",
+    },
+    black: {
+      textDecoration: "none",
+      color: "black",
     },
   });
 
   const classes = useStyles();
 
   return (
-    <div className="Portfolio">
+    <div className={classes.portfolio}>
       <Navbar />
       <div className={classes.title}>
         <Typography className={classes.text} variant="h3">
           Portfolio
         </Typography>
-        <Icon className={classes.icon}>add_circle_outline</Icon>
+        <Link to="/addPortfolio" className={classes.black}>
+          <Icon className={classes.icon}>add_circle_outline</Icon>
+        </Link>
       </div>
 
       <Grid container className={classes.wrap}>
-        {portfolio.map((item) => (
-          <Grid className={classes.container} item xs={6}>
+        {Object.keys(portfolio).map((item) => (
+          <Grid className={classes.container} item md={6} xs={12}>
             <Card
-              coin={item.coin}
-              symbol={item.symbol}
-              icon={item.icon}
-              cost={item.cost}
-              value={item.value}
-              amountBought={item.amountBought}
-              totalGain={item.totalGain}
+              coin={portfolio[item].coin}
+              symbol={portfolio[item].symbol}
+              icon={portfolio[item].icon}
+              cost={
+                localStorage.getItem(portfolio[item].coin + "Cost")
+                  ? localStorage.getItem(portfolio[item].coin + "Cost")
+                  : 0
+              }
+              value={
+                localStorage.getItem(portfolio[item].coin + "Amount")
+                  ? localStorage.getItem(portfolio[item].coin + "Amount") * price
+                  : 0
+              }
+              amountBought={
+                localStorage.getItem(portfolio[item].coin + "Amount")
+                  ? localStorage.getItem(portfolio[item].coin + "Amount")
+                  : 0
+              }
+              totalGain={
+                localStorage.getItem(portfolio[item].coin + "Cost")
+                  ? localStorage.getItem(portfolio[item].coin + "Amount") * price -
+                    localStorage.getItem(portfolio[item].coin + "Cost")
+                  : 0
+              }
             />
           </Grid>
         ))}
